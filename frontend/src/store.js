4aa10dd3283fb42c31ef7d0c3383040caed00b3d
Shubham Raj <shubham14p3@gmail.com>
2020-09-10 22:31:01 +0530

    added store for redux

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,12 @@
-import {createStore,combineReducers, compose, applyMiddleware} from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import {productListReducer} from './reducers/productReducers'
+import { productListReducer } from './reducers/productReducers';
 
-const initialState={};
-const reducer=combineReducers({
-    productList: productListReducer,
+
+const initialState = {};
+const reducer = combineReducers({
+  productList: productListReducer,
 })
-
-const composeEnhancer=window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__||compose;
-const store=createStore(reducer,initialState,composeEnhancer(applyMiddleware(thunk)));
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
 export default store;
