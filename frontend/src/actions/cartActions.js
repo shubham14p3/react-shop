d42f3b8cdd1ea9a3fc6368d889b860d0a2d14134
Shubham Raj <shubham14p3@gmail.com>
2020-10-07 01:16:16 +0530

    modifed format

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -6,27 +6,28 @@ const addToCart = (productId, qty) => async (dispatch, getState) => {
   try {
     const { data } = await Axios.get("/api/products/" + productId);
     dispatch({
-      type: CART_ADD_ITEM, payload: {
+      type: CART_ADD_ITEM,
+      payload: {
         product: data._id,
         name: data.name,
         image: data.image,
         price: data.price,
         countInStock: data.countInStock,
-        qty
-        
-      }
+        qty,
+      },
     });
-    const { cart: { cartItems } } = getState();
+    const {
+      cart: { cartItems },
+    } = getState();
     Cookie.set("cartItems", JSON.stringify(cartItems));
-
-  } catch (error) {
-
-  }
-}
+  } catch (error) {}
+};
 const removeFromCart = (productId) => (dispatch, getState) => {
   dispatch({ type: CART_REMOVE_ITEM, payload: productId });
 
-  const { cart: { cartItems } } = getState();
+  const {
+    cart: { cartItems },
+  } = getState();
   Cookie.set("cartItems", JSON.stringify(cartItems));
-}
-export { addToCart, removeFromCart }
+};
+export { addToCart, removeFromCart };
